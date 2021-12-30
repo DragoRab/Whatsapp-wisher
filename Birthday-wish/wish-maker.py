90aabd745692bd24c0b339d706b4e84d16954f5a
DragoRab <89995725+DragoRab@users.noreply.github.com>
2021-12-30 23:01:38 +0530

    added new year wish
    HAPPY NEW YEAR 2022

--- Birthday-wish/wish-maker.py
+++ Birthday-wish/wish-maker.py
@@ -1,17 +1,43 @@
 import Bday_wapp_char as wish
-name=input("Enter the name: ")
-name=name.replace(" ","")
-name= name.upper()
-l=len(name)
-if l<14:
+def main():
+    name=input("Enter the name: ")
+    name=name.replace(" ","")
+    name= name.upper()
+    l=len(name)
+    if l<14:
         name+='#'*(25-l)
-if l<10:
+    ch=int(input("1. new year\n2. B'day\nenter choice: "))
+
+    if ch==1:
+        k=int(input("enter w: "))
+        nyr(name,k)
+        
+    else:
+        bday(name,l)
+
+
+
+def bday(name,l):
+    if l<10:
+        wish.happy(name)
+        wish.wishort(name)
+    if l>=10:
+        wish.happy(name)
+        wish.wilong(name)
+
+def nyr(name,k):
     wish.happy(name)
-    wish.wishort(name)
-if l>=10:
-    wish.happy(name)
-    wish.wilong(name)
-    '''
+    if k==1:
+        wish.ny1(name)
+    else:
+        wish.ny2(name)
+    wish.y22(name)
+
+if __name__=='__main__':
+    main()
+
+
+'''
     Currently it can generate blocks of "HAPPY BIRTHDAY !'" 
     in monospace font for whatsapp
     and it is custom made to fit many whatsapp screen which can accomadate
@@ -21,18 +47,6 @@ if l>=10:
 
     I will update it with other blocks, when I get time, feel free to use and wish your friends& family
     Run this program with Bday_wapp_char.
-
-````
-|#####|        |#####|
- |###|          |###| 
- |###|          |###| 
- |###|  ______  |###| 
- |###|  /####\  |###| 
- |###| /######\ |###| 
- |###|/###/\###\|###| 
- |#######/  \###\###| 
- |######/    \######| 
- |#####/      \#####|
-
-```
-    '''
+    #####################
+    ABCDEFGHIJKLMNOPQRSTUV
+'''
